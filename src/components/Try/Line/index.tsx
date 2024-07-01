6b1ffeadff904760f0041730e92cb50dffff5a5e
udnlinyi <130973011+udnlinyi@users.noreply.github.com>
2024-07-01 10:09:11 +0800

    update component name

--- src/components/Try/Line/index.tsx
+++ src/components/Try/Line/index.tsx
@@ -4,36 +4,36 @@ import randomPicture from "@utils/randomPicture";
 import randomText from "@utils/randomText";
 
 export type Card = {
-  title: string;
-  description: string;
-  image: string;
+	title: string;
+	description: string;
+	image: string;
 };
 
 export type Cards = Card[];
 
 const cards = [
-  {
-    title: randomText(5, 10),
-    description: randomText(20, 30),
-    image: randomPicture(),
-  },
-  {
-    title: randomText(5, 10),
-    description: randomText(20, 30),
-    image: randomPicture(),
-  },
-  {
-    title: randomText(5, 10),
-    description: randomText(20, 30),
-    image: randomPicture(),
-  },
+	{
+		title: randomText(5, 10),
+		description: randomText(20, 30),
+		image: randomPicture(),
+	},
+	{
+		title: randomText(5, 10),
+		description: randomText(20, 30),
+		image: randomPicture(),
+	},
+	{
+		title: randomText(5, 10),
+		description: randomText(20, 30),
+		image: randomPicture(),
+	},
 ];
 
-export default function index() {
-  return (
-    <div>
-      <p>Line</p>
-      <List {...{ cards }} />
-    </div>
-  );
+export default function Index() {
+	return (
+		<div>
+			<p>Line</p>
+			<List {...{ cards }} />
+		</div>
+	);
 }
