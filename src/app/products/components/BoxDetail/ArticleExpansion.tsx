4cb7d782903733528960e9902a6a793c6fbce900
udnlinyi <130973011+udnlinyi@users.noreply.github.com>
2024-07-04 19:03:59 +0800

    update products page style

--- src/app/products/components/BoxDetail/ArticleExpansion.tsx
+++ src/app/products/components/BoxDetail/ArticleExpansion.tsx
@@ -18,11 +18,11 @@ export default function ArticleExpansion({ article }: { article: string }) {
 			<section
 				className={
 					"mt-[-40px] overflow-hidden transition-all delay-500 duration-0 ease-linear " +
-					`${!isExpanded ? " h-[120px]" : " h-16 opacity-0"}`
+					`${!isExpanded ? " h-[122px]" : " h-16 opacity-0"}`
 				}
 			>
 				<div
-					className="h-16 border-b border-landscape-450"
+					className="group h-16 border-b border-landscape-450"
 					style={{
 						backgroundImage:
 							"linear-gradient(to top, var(--landscape-300-rgb), rgba(0,0,0,0))",
@@ -32,10 +32,10 @@ export default function ArticleExpansion({ article }: { article: string }) {
 					className="group cursor-pointer text-center"
 					onClick={() => setIsExpanded(true)}
 				>
-					<p className="pt-2 font-normal text-ash-850 transition-all duration-200 group-active:text-accent-300">
+					<p className="pb-[6px] pt-2 font-normal text-ash-850 transition-all duration-200 group-active:text-accent-300">
 						閱讀更多
 					</p>
-					<i className="i-arrow7-down inline-block scale-125 text-ash-900 transition-all duration-200 group-active:text-accent-300"></i>
+					<i className="i-arrow7-down inline-block translate-y-[-25%] text-xl text-ash-900 transition-all duration-200 group-hover:animate-bounce group-active:text-accent-300"></i>
 				</div>
 			</section>
 		</div>
