5e29328ff8b1d9b74cbbe1f3243a249b7b78cc82
udnlinyi <130973011+udnlinyi@users.noreply.github.com>
2024-08-02 11:12:39 +0800

    update cate via fetched 2

--- src/app/(main)/cate/page.tsx
+++ src/app/(main)/cate/page.tsx
@@ -1,8 +1,7 @@
 import { redirect } from "next/navigation";
-import { categoryDatas } from "@lib/data";
+//import { categoryDatas } from "@lib/data";
 
 export default function Page() {
-	const firstCate = categoryDatas[0];
-	redirect(firstCate.url);
+	redirect("/cate/1");
 	return null;
 }
