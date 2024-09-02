6b761c0bb1010c32f0ae077e0a2f54dddbac7cc2
udnlinyi <130973011+udnlinyi@users.noreply.github.com>
2024-09-02 14:40:09 +0800

    update publishers page style

--- src/app/(main)/publishers/page.tsx
+++ src/app/(main)/publishers/page.tsx
@@ -15,7 +15,11 @@ import randomText from "@tools/randomText";
 
 import NewsSlider from "@/components/slider/NewsSlider";
 import Breadcrumb from "@/components/Breadcrumb";
-import { UiTitle } from "@/components/customUI";
+import { UiTitle, UiSection } from "@/components/customUI";
+import { UiButton } from "@/components/customUI/client";
+
+import FreeGlide from "@/components/customUI/FreeGlide";
+import NestedLink from "@/components/customUI/NestedLink";
 
 //
 const newsArray: NewsType[] = Array.from({ length: 5 }, (_, i) => i + 1).map(
@@ -24,6 +28,13 @@ const newsArray: NewsType[] = Array.from({ length: 5 }, (_, i) => i + 1).map(
 		text: randomText(40, 80),
 	})
 );
+
+const publishers = Array.from({ length: 46 }, (_, i) => i + 1).map((i) => ({
+	id: new Date().getTime() + i,
+	title: randomText(3, 20),
+	link: "",
+	picture: randomPicture(),
+}));
 
 //
 export default async function Page() {
@@ -46,10 +57,29 @@ export default async function Page() {
 			<section className="pb-5 max-md:hidden">
 				<Breadcrumb />
 			</section>
-			<section className="mb-16 px-6">
-				{/* -- */}
-				<div className="mb-5 flex items-center justify-start pb-2 pt-3 md:gap-4">
-					<UiTitle className="">出版商</UiTitle>
+			<section>
+				<UiTitle className="mb-4 max-md:px-5">出版社專區</UiTitle>
+				<div className="mx-auto flex w-[calc(180*6px+30*5px)] flex-wrap content-start justify-start gap-[30px] max-xl:w-[calc(180*4px+30*3px)] max-lg:w-[calc(180*3px+30*2px)] max-md:w-[calc(160*2px+20*1px)] max-md:gap-5">
+					{publishers.map((publisher) => (
+						<div
+							key={publisher.id}
+							className="group w-[180px] max-md:w-[160px]"
+						>
+							<Link
+								href={publisher.link}
+								className="block w-full transition-transform group-hover:-translate-y-4"
+							>
+								<picture className="pic-base h-[95px] w-[180px] rounded max-md:h-[84px] max-md:w-[160px]">
+									<img src={publisher.picture} alt="" />
+								</picture>
+								<div className="py-[10px]">
+									<p className="line-clamp-1 w-full text-center text-lg font-normal text-ash-900 group-hover:text-accent-300 group-active:text-accent-220 max-md:text-base">
+										{publisher.title}
+									</p>
+								</div>
+							</Link>
+						</div>
+					))}
 				</div>
 			</section>
 		</section>
